6c543119424681ed83329cdee3fcd53ee81dce7d
yanshuyu <zhishuyan@gmail.com>
2019-10-11 13:05:32 +0800

    add adaptive ui demo.

---
+++ adaptiveUI/adaptiveUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  adaptiveUI
+//
+//  Created by sy on 2019/10/11.
+//  Copyright © 2019 sy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
